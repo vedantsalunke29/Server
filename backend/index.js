f853c577e7f49f773d6ca2603e723bebd954452b
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-09-21 13:53:44 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const corsConfig = {
-  origin: ['https://one-pict.vercel.app', 'http://localhost:5173'],
+  origin: ['*'],
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credential: true
 };
