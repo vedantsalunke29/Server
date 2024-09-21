6b82870db204ca3257b1c817ec146642451f4144
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-09-21 13:33:00 +0530

    z

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const uri = process.env.MONGO_URI;
+        await mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true});
         console.log(`Successfully connected to mongoDB`);
     } catch (error) {
         console.error(`ERROR: ${error}`);
