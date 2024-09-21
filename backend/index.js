580858ba283f30576d92a4039a36d97b256c8205
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-09-21 15:10:46 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const corsConfig = {
-  origin: ['*'],
+  origin: '*',
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credential: true
 };
